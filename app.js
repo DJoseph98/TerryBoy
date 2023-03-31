f41d7819c6560937f567d947e84154508f0f357c
Terence Rajapaksage <terenceraja@hotmail.fr>
2023-04-01 00:23:31 +0200

    fini

---
+++ app.js
@@ -0,0 +1,27 @@
+document.querySelector("#submit").addEventListener("click",
+function() {
+    console.log("click")
+let name = document.querySelector("#name").value
+let surname = document.querySelector("#surname").value
+let age =  document.querySelector("#age").value
+let e = document.querySelector("#friends");
+let text = e.options[e.selectedIndex].text;
+
+console.log(`Your name is ${name}`);
+console.log(`Your surname ${surname}`);
+console.log(`Your age is ${age}`);
+console.log(`Your friend is ${text}`);
+
+
+
+document.querySelector(".container").innerHTML +=
+
+    `<div>
+      <h2>Your name is ${name}</h2>
+      <h2>Your surname is ${surname}</h2>
+       <h2>Your age is ${age}</h2>
+       <h2>Your Friend is ${text}</h2>
+    </div>
+    `;
+}
+)
